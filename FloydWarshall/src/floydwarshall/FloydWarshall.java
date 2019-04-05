0ea2f3a26ae572f983252b0a72823e8925c11910
Sebastian Alejandro Tavera Guarin <taveras@CampusUninorte.local>
2019-04-05 14:03:19 -0500

    commit terminado

--- FloydWarshall/src/floydwarshall/FloydWarshall.java
+++ FloydWarshall/src/floydwarshall/FloydWarshall.java
@@ -5,6 +5,8 @@
  */
 package floydwarshall;
 
+import static jdk.nashorn.internal.objects.Global.print;
+
 /**
  *
  * @author taveras
@@ -14,8 +16,65 @@ public class FloydWarshall {
     /**
      * @param args the command line arguments
      */
+    static void floydWarshall(int grafo[][]){
+        int n= grafo.length;
+    int distancia[][]=getMatriz(0,grafo);
+    int caminos[][]=getMatriz(1,grafo);
+    int tmp;
+    print("Matriz de distancias inicial: ", distancia);
+    print("Matriz de caminos inicial", caminos);
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tmp=distancia[i][k]+ distancia[k][j];
+                    if (tmp<distancia[i][j]) {
+                        distancia[i][j]=tmp;
+                        caminos[i][j]=k;
+                        
+                    }
+                }
+            }
+    
+        }
+        print("Matriz de distancias final: ", distancia);
+        print("Matriz de caminos final", caminos);
+    }
+    
+    
+    static int[][] getMatriz(int tipo,int grafo[][]){
+    int n= grafo.length;
+    int M[][]= new int [n][n];
+        if (tipo==1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    M[i][j]=j;
+                    
+                }
+            }
+        }else{
+        int INF=9999;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i!=j && grafo[i][j]==0) {
+                        M[i][j]=INF;
+                        
+                    }else{
+                    M[i][j]=grafo[i][j];
+                    }
+                }
+            }
+        }
+        return M;
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        int grafo[][]=
+        {
+              {0,2,0,2,0},
+              {3,0,0,2,0},
+              {8,0,0,2,1},
+              {8,7,0,0,0},
+              {0,3,0,2,0},
+        };
     }
     
 }
